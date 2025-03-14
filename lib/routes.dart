95ec91b1c06e8d86de24bdd33d388fb60cd749cd
Navid Rahman <navidrahman92@gmail.com>
2025-03-15 00:28:55 +0600

    set up routes

---
+++ lib/routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen.dart';
+import 'screens/splash_screen.dart';
+
+Map<String, Widget Function(BuildContext)> routes = {
+  SplashScreen.routeName: (context) => const SplashScreen(),
+  HomeScreen.routeName: (context) => const HomeScreen(),
+};
