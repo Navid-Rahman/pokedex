7ff667e91b400f3e26d39193697cb297673af6f7
Navid Rahman <navidrahman92@gmail.com>
2025-03-15 00:56:58 +0600

    home screen initial setup added

--- lib/core/assets.dart
+++ lib/core/assets.dart
@@ -2,4 +2,6 @@ class Assets {
   const Assets._();
 
   static const String kSplashScreen = 'assets/contents/splash_screen.png';
+  static const String kAppTitle = 'assets/contents/pokedex_title.svg';
+  static const String kPokeBallIcon = 'assets/contents/pokeball.svg';
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '/core/app_colors.dart';
+import '/core/assets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,7 +12,18 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Screen")),
+      backgroundColor: AppColors.primaryColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.primaryColor,
+        leading: SvgPicture.asset(
+          Assets.kPokeBallIcon,
+          colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcATop),
+        ),
+        title: SvgPicture.asset(
+          Assets.kAppTitle,
+          height: 30,
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
