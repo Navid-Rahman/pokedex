09a66cd25e3f220b09821ab7dbc637cc0e11f3fd
Navid Rahman <navidrahman92@gmail.com>
2025-03-15 01:35:43 +0600

    added font - Poppins all over the application

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'PokéDex',
       routes: routes,
+      theme: ThemeData(
+        // Set Poppins as the default font family
+        fontFamily: 'Poppins', // Match the family name from pubspec.yaml
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(),
+          bodyMedium: TextStyle(),
+          bodySmall: TextStyle(),
+          headlineLarge: TextStyle(),
+          headlineMedium: TextStyle(),
+          headlineSmall: TextStyle(),
+        ).apply(fontFamily: 'Poppins'),
+
+        useMaterial3: true,
+      ),
       initialRoute: SplashScreen.routeName,
     );
   }
