439e476688f06b6e15da8106f5ef2b6935461feb
Navid Rahman <navidrahman92@gmail.com>
2025-03-15 00:29:13 +0600

    set up splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../core/assets.dart';
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  static const String routeName = '/splash';
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  Future<void> _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 2));
+    if (mounted) {
+      Navigator.of(context).pushReplacementNamed(HomeScreen.routeName);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset(
+        Assets.kSplashScreen,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
