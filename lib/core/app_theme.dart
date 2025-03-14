a58e9c5a1ce8c401913e689f57e4c18f4867431a
Navid Rahman <navidrahman92@gmail.com>
2025-03-15 01:51:28 +0600

    added global theme #2

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get theme => ThemeData(
+        fontFamily: 'Poppins',
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 96),
+          displayMedium: TextStyle(fontSize: 80),
+          displaySmall: TextStyle(fontSize: 72),
+          headlineLarge: TextStyle(fontSize: 64),
+          headlineMedium: TextStyle(fontSize: 56),
+          headlineSmall: TextStyle(fontSize: 48),
+          titleLarge: TextStyle(fontSize: 44),
+          titleMedium: TextStyle(fontSize: 40),
+          titleSmall: TextStyle(fontSize: 36),
+          labelLarge: TextStyle(fontSize: 28),
+          labelMedium: TextStyle(fontSize: 24),
+          labelSmall: TextStyle(fontSize: 20),
+          bodyLarge: TextStyle(fontSize: 20),
+          bodyMedium: TextStyle(fontSize: 16),
+          bodySmall: TextStyle(fontSize: 12),
+        ).apply(fontFamily: 'Poppins'),
+        useMaterial3: true,
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'core/app_theme.dart';
 import 'routes.dart';
 import 'screens/splash_screen.dart';
 
@@ -15,27 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'PokéDex',
       routes: routes,
-      theme: ThemeData(
-        fontFamily: 'Poppins',
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(fontSize: 96),
-          displayMedium: TextStyle(fontSize: 80),
-          displaySmall: TextStyle(fontSize: 72),
-          headlineLarge: TextStyle(fontSize: 64),
-          headlineMedium: TextStyle(fontSize: 56),
-          headlineSmall: TextStyle(fontSize: 48),
-          titleLarge: TextStyle(fontSize: 44),
-          titleMedium: TextStyle(fontSize: 40),
-          titleSmall: TextStyle(fontSize: 36),
-          labelLarge: TextStyle(fontSize: 28),
-          labelMedium: TextStyle(fontSize: 24),
-          labelSmall: TextStyle(fontSize: 20),
-          bodyLarge: TextStyle(fontSize: 20),
-          bodyMedium: TextStyle(fontSize: 16),
-          bodySmall: TextStyle(fontSize: 12),
-        ).apply(fontFamily: 'Poppins'),
-        useMaterial3: true,
-      ),
+      theme: AppTheme.theme,
       initialRoute: SplashScreen.routeName,
     );
   }
