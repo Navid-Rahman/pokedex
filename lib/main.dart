f53e08c066dc69a74227533b0f1f7ac5b855f777
Navid Rahman <navidrahman92@gmail.com>
2025-03-20 02:06:43 +0600

    firebase setup added for android, ios, web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'core/app_theme.dart';
+import 'env/env.dart';
 import 'routes.dart';
 import 'screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: Env.firebaseApiKey,
+        authDomain: Env.firebaseAuthDomain,
+        projectId: Env.firebaseProjectId,
+        storageBucket: Env.firebaseStorageBucket,
+        messagingSenderId: Env.firebaseMessagingSenderId,
+        appId: Env.firebaseAppId,
+        measurementId: Env.firebaseMeasurementId,
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
